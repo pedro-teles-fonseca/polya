dfbc81c0792432cc1b033da91823919734c49970
Pedro Teles da Fonseca <pedro.teles.fonseca@outlook.com>
2020-05-13 23:54:37 +0100

    Improve Bayes factors functions

--- R/bayes-factors.R
+++ R/bayes-factors.R
@@ -1,13 +1,17 @@
 
-# Bayes factor
+# Multinomial - Dirichlet model: Bayes factor
 # ------------------------------------------
 bfactor.multinomial <- function(
-  data,
+  x,
   null.par,
-  alpha = rep.int(1, length(null.par))) {
+  alpha = rep.int(1, length(null.par)),
+  transf = "level") {
 
-  if(any(!is.atomic(data), !is.vector(data))){
-    stop("Invalid argument: 'data' must be an atomic vector.")
+  if(any(!is.atomic(x), !is.vector(x))){
+    stop("Invalid argument: 'x' must be an atomic vector.")
+  }
+  if(isFALSE(typeof(x) %in% c("double", "integer", "character"))){
+    stop("Invalid argument: typeof(x) must be 'integer', 'double' or 'character'.")
   }
   if (any(!is.atomic(null.par), !is.vector(null.par))){
     stop("Invalid argument: 'null.par' must be an atomic vector.")
@@ -21,26 +25,25 @@ bfactor.multinomial <- function(
   if(any(!is.numeric(alpha), any(alpha < 0), any(is.na(alpha)), is.null(alpha))){
     stop("Invalid argument: 'alpha' must be non-negative.")
   }
-  # if (model == "multinomial") {
-  #   if (length(null.par) == 1) {
+  if(isFALSE(transf %in% c("level", "log", "log10"))){
+    stop("Invalid argument: 'transf' must be either 'level', 'log' or 'log10'.")
+  }
+
+  # if (length(null.par) == 1) {
   #     stop("One parameter model. Use 'model = 'binomial'.")
   #   } else {
-  #     if (length(null.par) < length(alpha)) {
-  #       stop(
-  #         "Invalid model specification: more hyper-parameters than null parameter values."
-  #       )
-  #       } else if (length(alpha) == 1){
-  #         alpha <- rep.int(alpha, length(null.par))
-  #     } else if(length(null.par) > length(alpha)) {
-  #       stop(
-  #         "Invalid model specification: more null parameter values than hyper parameters."
-  #       )
-  #     }
-  #   }
-  # }
+  if (length(null.par) < length(alpha)) {
+      stop("Invalid model specification: more hyper-parameters than null parameter values.")
+    } else if (length(alpha) == 1){
+          alpha <- rep.int(alpha, length(null.par))
+    } else if(length(null.par) > length(alpha)) {
+      stop(
+          "Invalid model specification: more null parameter values than hyper parameters."
+        )
+    }
 
-  data <- data[!is.na(data)]
-  x <- table(data)
+  x <- x[!is.na(x)]
+  x <- table(x)
 
     bfactor <- function(x, null.par, alpha) {
 
@@ -50,23 +53,24 @@ bfactor.multinomial <- function(
       exp(log.bfactor)
     }
 
-    bfactor(x, null.par, alpha)
+    c("Bayes factor in favour of H0" = bfactor(x, null.par, alpha))
 }
 
-
-
-# Bayes factor
+# Binomial - Beta model: Bayes factor
 # ------------------------------------------
-bfactor <- function(
+bfactor.binomial <- function(
   data,
   null.par,
-  model = "multinomial",
-  alpha = rep.int(1, length(null.par)),
+  sucess,
   a = 1,
-  b = 1) {
+  b = 1,
+  transf = "level") {
 
-  if(any(!is.atomic(data), !is.vector(data))){
+  if(any(!is.atomic(data), !is.vector(data), is.null(data))){
     stop("Invalid argument: 'data' must be an atomic vector.")
+  }
+  if(isFALSE(typeof(data) %in% c("double", "integer", "character"))){
+    stop("Invalid argument: typeof(data) must be 'integer', 'double' or 'character'.")
   }
   if (any(!is.atomic(null.par), !is.vector(null.par))){
     stop("Invalid argument: 'null.par' must be an atomic vector.")
@@ -74,60 +78,28 @@ bfactor <- function(
   if(any(!is.numeric(null.par), null.par >= 1, null.par <= 0, is.na(null.par), is.null(null.par))) {
     stop("Invalid argument: all 'null.par' values must be between 0 and 1.")
   }
-  if (any(isFALSE(model %in% c("multinomial", "binomial")),  is.null(model))) {
-    stop("Invalid argument: 'model' must be either 'multinomial' or 'binomial'.")
-  }
-  if(any(!is.atomic(alpha), !is.vector(alpha))){
-    stop("Invalid argument: 'alpha' must be an atomic vector.")
-  }
-  if(any(!is.numeric(alpha), any(alpha < 0), any(is.na(alpha)), is.null(alpha))){
-    stop("Invalid argument: 'alpha' must be non-negative.")
+  if(isFALSE(sucess %in% unique(data))){
+    stop("Invalid argument: 'sucess %in% unique(data)' must be TRUE.")
   }
   if(any(!is.atomic(a), !is.atomic(b), !is.vector(a), !is.vector(b))){
-    stop("Invalid argument: 'alpha' must be an atomic vector.")
+    stop("Invalid argument: 'a' and 'b' must be atomic vectors.")
   }
   if(any(a <= 0, b <= 0, is.na(a), is.na(b), is.null(a), is.null(b))){
     stop("Invalid argument: all values of 'a' and 'b' must be greather than 0.")
   }
-
-  if (model == "multinomial") {
-    if (length(null.par) == 1) {
-      stop("One parameter model. Use 'model = 'binomial'.")
-    } else {
-      if (length(null.par) < length(alpha)) {
-        stop(
-          "Invalid model specification: more hyper-parameters than null parameter values."
-        )
-      } else if (length(alpha) == 1){
-        alpha <- rep.int(alpha, length(null.par))
-      } else if(length(null.par) > length(alpha)) {
-        stop(
-          "Invalid model specification: more null parameter values than hyper parameters."
-        )
-      }
-    }
+  if(any(isFALSE(length(null.par) == 1), isFALSE(length(sucess) == 1))){
+    stop("Invalid argument: 'null.par' and 'sucess' must be of length 1")
+  }
+  if(any(isFALSE(length(a) == 1), isFALSE(length(b) == 1))){
+    stop("Invalid argument: 'a' and 'b' must be of length 1")
+  }
+  if(isFALSE(transf %in% c("level", "log", "log10"))){
+    stop("Invalid argument: 'transf' must be either 'level', 'log' or 'log10'.")
   }
 
   data <- data[!is.na(data)]
-
-  if (model == "multinomial") {
-
-    x <- table(data)
-
-    bfactor <- function(x, null.par, alpha) {
-
-      log.bfactor <- sum(x * log(null.par)) + sum(lgamma(alpha)) + lgamma(sum(alpha + x)) -
-        lgamma(sum(alpha)) - sum(lgamma(alpha + x))
-
-      exp(log.bfactor)
-    }
-
-    bfactor(x, null.par, alpha)
-
-  } else if (model == "binomial") {
-
-    x <- rev(table(data))[1]
-    n <- length(data)
+  x <- as.numeric(table(data == sucess)["TRUE"])
+  n <- length(data)
 
     bfactor <- function(x, null.par, a, b, n)  {
 
@@ -137,8 +109,14 @@ bfactor <- function(
       exp(log.bfactor)
     }
 
-    c("Bayes factor in favour of H0" = bfactor(x, null.par, a, b, n))
-  }
+    if(transf == "log"){
+      c("Bayes factor in favour of H0" = log(bfactor(x, null.par, a, b, n)))
+    } else if(transf == "log10"){
+      c("Bayes factor in favour of H0" = log10(bfactor(x, null.par, a, b, n)))
+    } else {
+      c("Bayes factor in favour of H0" = bfactor(x, null.par, a, b, n))
+    }
+
 }
 
 
