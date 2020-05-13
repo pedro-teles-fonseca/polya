7df57af926095a9b614fc4c5f0ba955eba3c6623
Pedro Teles da Fonseca <pedro.teles.fonseca@outlook.com>
2020-05-13 16:29:59 +0100

    Include function to compute mode

--- R/auxiliary-functions.R
+++ R/auxiliary-functions.R
@@ -2,3 +2,13 @@
 posterior.prob <- function(pi_null, bf) {
   ((1 + ((1 - pi_null) / pi_null) * (1 / bf))) ^ (-1)
 }
+
+# Function to compte the mode of a vector.
+
+getmode <- function(v) {
+
+  uniqv <- unique(v)
+
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+
+}
