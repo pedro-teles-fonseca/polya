7f7f67b629c1e3ebdf3a6dcfcc3846007a1a2b7c
Pedro Teles da Fonseca <pedro.teles.fonseca@outlook.com>
2020-05-13 05:32:53 +0100

    Create function to test many binomial hypotheses at the same time

--- R/hypothesis-testing.R
+++ R/hypothesis-testing.R
@@ -1,7 +1,8 @@
 
 # Testing Multinomial and Binomial point null hypotheses
 # ------------------------------------------
-test.point.null <- function(data,
+test.point.null <- function(
+  data,
   null.par,
   pi_null = 0.5,
   model = "multinomial",
@@ -11,6 +12,8 @@ test.point.null <- function(data,
   probs.round = 3){
 
   bf <- bayes.factor(data = data, null.par = null.par, model = model, hyper.par = hyper.par)
+
+  # data <- as.vector(data[!is.na(data)])
 
   if(model == "multinomial"){
     p_value <- chisq.test(table(data), p = null.par)$p.value
@@ -39,8 +42,59 @@ test.point.null <- function(data,
 
 # Testing many binomial point null hypotheses
 # ------------------------------------------
+test.binomial.hypotheses <- function( # Function to compute Binomial-Beta model results (BL1 or BL2)
+  data,
+  null.par,
+  pi_null = rep.int(0.5, length(null.par)),
+  hyper.par = list(rep(1, length(null.par)), rep(1, length(null.par))),
+  log10 = TRUE,
+  bf.round = 2,
+  probs.round = 3){
 
-test.binomial.hypotheses <- function(){}
+    x <- as.numeric(table(msdigit(data)))
+    n <- length(msdigit(data))
+    obs_prop <- as.numeric(rev(table(data))[1])/n
+    z_stats <- (abs(obs_prop-null.par)-(1/(2*n)))/sqrt((null.par*(1-null.par))/n)
+    p_values <- 2 * (1-pnorm(abs(z_stats), 0, 1))
+
+    digits <- 1:9
+
+    bf <- function(x, theta_null, a, b){
+      exp(
+        x * log(theta_null) + (n - x) * log(1-theta_null) + lgamma(a) + lgamma(b) + lgamma(n + a + b) -
+          (lgamma(a + b) + lgamma(n + a - x) + lgamma(x + a))
+      )
+    }
+
+    bf <- mapply(bf, x, null.par, hyper.par[[1]], hyper.par[[2]])
+
+
+  PostProb.H0 <- mapply(FUN = posterior.prob, pi_null = pi_null, bf = bf)
+
+  result <- data.frame(
+    "Digit" = digits,
+    "BayesFactors" = round(if(log10 == FALSE){bf} else{log10(bf)}, bf.round),
+    "Evidence" = ifelse(bf < 1, "Negative",
+      ifelse(bf < 3.2, "Weak",
+        ifelse(bf < 10, "Substantial",
+          ifelse(bf < 100, "Strong",
+            "Decisive")))
+    ),
+    "PostProb.H0" = round(PostProb.H0, probs.round),
+    "LB.PostProb.H0" = round(pcal(p_values), probs.round),
+    "P.value" = p_values
+  )
+
+  if(log10 == TRUE){names(result) <- gsub("BayesFactors", "log10(BayesFactors)", names(result))}
+  return(result)
+
+}
+
+# test.binomial.hypotheses(Portugal.bl1, theta_benford(1))
+# bayes.factor(Portugal.bl1, theta_benford, model = "binomial")
+
+
+
 
 
 
