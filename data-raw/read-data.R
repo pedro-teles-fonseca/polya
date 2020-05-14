99acb8aede213402fdf78f674669f4d7f224f8ac
Pedro Teles da Fonseca <pedro.teles.fonseca@outlook.com>
2020-05-14 19:26:57 +0100

    data updated

--- data-raw/read-data.R
+++ data-raw/read-data.R
@@ -5,33 +5,64 @@ library(digit.analysis)
 data <- read.delim("data-raw/data.txt")
 
 for (c in 1:ncol(data)) {
-  assign(paste0(names(data)[c], ".bl1"), msdigit(data[, c]))
-  assign(paste0(names(data)[c], ".bl2"), smsdigit(data[, c]))
+  assign(paste0(tolower(names(data)[c]), ".bl1"), msdigit(data[, c]))
+  assign(paste0(tolower(names(data)[c]), ".bl2"), smsdigit(data[, c]))
 }
 
+datalist.bl1 <- list(
+  austria.bl1,
+  belgium.bl1,
+  finland.bl1,
+  france.bl1,
+  germany.bl1,
+  greece.bl1,
+  ireland.bl1,
+  italy.bl1,
+  luxembourg.bl1,
+  netherlands.bl1,
+  portugal.bl1,
+  spain.bl1
+)
+
+datalist.bl2 <- list(
+  austria.bl2,
+  belgium.bl2,
+  finland.bl2,
+  france.bl2,
+  germany.bl2,
+  greece.bl2,
+  ireland.bl2,
+  italy.bl2,
+  luxembourg.bl2,
+  netherlands.bl2,
+  portugal.bl2,
+  spain.bl2
+)
 usethis::use_data(
-  Austria.bl1,
-  Belgium.bl1,
-  Finland.bl1,
-  France.bl1,
-  Germany.bl1,
-  Greece.bl1,
-  Ireland.bl1,
-  Italy.bl1,
-  Luxembourg.bl1,
-  Netherlands.bl1,
-  Portugal.bl1,
-  Spain.bl1,
-  Austria.bl2,
-  Belgium.bl2,
-  Finland.bl2,
-  France.bl2,
-  Germany.bl2,
-  Greece.bl2,
-  Ireland.bl2,
-  Italy.bl2,
-  Luxembourg.bl2,
-  Netherlands.bl2,
-  Portugal.bl2,
-  Spain.bl2,
+  austria.bl1,
+  belgium.bl1,
+  finland.bl1,
+  france.bl1,
+  germany.bl1,
+  greece.bl1,
+  ireland.bl1,
+  italy.bl1,
+  luxembourg.bl1,
+  netherlands.bl1,
+  portugal.bl1,
+  spain.bl1,
+  austria.bl2,
+  belgium.bl2,
+  finland.bl2,
+  france.bl2,
+  germany.bl2,
+  greece.bl2,
+  ireland.bl2,
+  italy.bl2,
+  luxembourg.bl2,
+  netherlands.bl2,
+  portugal.bl2,
+  spain.bl2,
+  datalist.bl1,
+  datalist.bl2,
   overwrite = TRUE)
