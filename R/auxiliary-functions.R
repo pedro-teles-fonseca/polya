2c611fdb8b4405e76f7ffe7c7bbf7e90c9531159
Pedro Teles da Fonseca <pedro.teles.fonseca@outlook.com>
2020-05-14 19:26:17 +0100

    Add comments

--- R/auxiliary-functions.R
+++ R/auxiliary-functions.R
@@ -1,12 +1,14 @@
 
-bfactor.to.prob <- function(pi_null, bf) {
+
+# Function to turn Bayes factors into posterior probabilities of the NULL
+
+bfactor.to.prob <- function(bf, pi_null = .5) {
   ((1 + ((1 - pi_null) / pi_null) * (1 / bf))) ^ (-1)
 }
 
-# Function to compte the mode of a vector.
+# Function to compute the mode of a vector.
 
 getmode <- function(v) {
-
   uniqv <- unique(v)
 
   uniqv[which.max(tabulate(match(v, uniqv)))]
