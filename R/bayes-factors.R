5f1a93edfa07513d3f2d2bef2bbad75c5bae009c
Pedro Teles da Fonseca <pedro.teles.fonseca@outlook.com>
2020-05-14 19:27:17 +0100

    Multiple updates

--- R/bayes-factors.R
+++ R/bayes-factors.R
@@ -42,35 +42,40 @@ bfactor.multinomial <- function(
         )
     }
 
-  x <- x[!is.na(x)]
-  x <- table(x)
+  counts <- table(x[!is.na(x)])
 
-    bfactor <- function(x, null.par, alpha) {
+    bfactor <- function(counts, null.par, alpha) {
 
-      log.bfactor <- sum(x * log(null.par)) + sum(lgamma(alpha)) + lgamma(sum(alpha + x)) -
-        lgamma(sum(alpha)) - sum(lgamma(alpha + x))
+      log.bfactor <- sum(counts * log(null.par)) + sum(lgamma(alpha)) + lgamma(sum(alpha + counts)) -
+        lgamma(sum(alpha)) - sum(lgamma(alpha + counts))
 
       exp(log.bfactor)
     }
 
-    c("Bayes factor in favour of H0" = bfactor(x, null.par, alpha))
+    if(transf == "log"){
+      c("log(BF)" = log(bfactor(counts, null.par, alpha)))
+    } else if(transf == "log10"){
+      c("log10(BF)" = log10(bfactor(counts, null.par, alpha)))
+    } else {
+      c("BF" = bfactor(counts, null.par, alpha))
+    }
 }
 
 # Binomial - Beta model: Bayes factor
 # ------------------------------------------
 bfactor.binomial <- function(
-  data,
+  x,
   null.par,
   sucess,
   a = 1,
   b = 1,
   transf = "level") {
 
-  if(any(!is.atomic(data), !is.vector(data), is.null(data))){
-    stop("Invalid argument: 'data' must be an atomic vector.")
+  if(any(!is.atomic(x), !is.vector(x), is.null(x))){
+    stop("Invalid argument: 'x' must be an atomic vector.")
   }
-  if(isFALSE(typeof(data) %in% c("double", "integer", "character"))){
-    stop("Invalid argument: typeof(data) must be 'integer', 'double' or 'character'.")
+  if(isFALSE(typeof(x) %in% c("double", "integer", "character"))){
+    stop("Invalid argument: typeof(x) must be 'integer', 'double' or 'character'.")
   }
   if (any(!is.atomic(null.par), !is.vector(null.par))){
     stop("Invalid argument: 'null.par' must be an atomic vector.")
@@ -78,8 +83,8 @@ bfactor.binomial <- function(
   if(any(!is.numeric(null.par), null.par >= 1, null.par <= 0, is.na(null.par), is.null(null.par))) {
     stop("Invalid argument: all 'null.par' values must be between 0 and 1.")
   }
-  if(isFALSE(sucess %in% unique(data))){
-    stop("Invalid argument: 'sucess %in% unique(data)' must be TRUE.")
+  if(isFALSE(sucess %in% unique(x))){
+    stop("Invalid argument: 'sucess %in% unique(x)' must be TRUE.")
   }
   if(any(!is.atomic(a), !is.atomic(b), !is.vector(a), !is.vector(b))){
     stop("Invalid argument: 'a' and 'b' must be atomic vectors.")
@@ -97,24 +102,24 @@ bfactor.binomial <- function(
     stop("Invalid argument: 'transf' must be either 'level', 'log' or 'log10'.")
   }
 
-  data <- data[!is.na(data)]
-  x <- as.numeric(table(data == sucess)["TRUE"])
-  n <- length(data)
+  x <- x[!is.na(x)]
+  sucesses <- as.numeric(table(x == sucess)["TRUE"])
+  n <- length(x)
 
-    bfactor <- function(x, null.par, a, b, n)  {
+    bfactor <- function(sucesses, null.par, a, b, n)  {
 
-      log.bfactor <- x * log(null.par) + (n - x) * log(1 - null.par) + lgamma(a) + lgamma(b) + lgamma(n + a + b) -
-        (lgamma(a + b) + lgamma(n + a - x) + lgamma(x + a))
+      log.bfactor <- sucesses * log(null.par) + (n - sucesses) * log(1 - null.par) + lgamma(a) + lgamma(b) + lgamma(n + a + b) -
+        (lgamma(a + b) + lgamma(n + a - sucesses) + lgamma(sucesses + a))
 
       exp(log.bfactor)
     }
 
     if(transf == "log"){
-      c("Bayes factor in favour of H0" = log(bfactor(x, null.par, a, b, n)))
+      c("log(BF)" = log(bfactor(sucesses, null.par, a, b, n)))
     } else if(transf == "log10"){
-      c("Bayes factor in favour of H0" = log10(bfactor(x, null.par, a, b, n)))
+      c("log10(BF)" = log10(bfactor(sucesses, null.par, a, b, n)))
     } else {
-      c("Bayes factor in favour of H0" = bfactor(x, null.par, a, b, n))
+      c("BF" = bfactor(sucesses, null.par, a, b, n))
     }
 
 }
