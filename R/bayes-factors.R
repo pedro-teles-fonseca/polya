7bd01c5a469d859e7b413587e89b18e528b05cd9
Pedro Teles da Fonseca <pedro.teles.fonseca@outlook.com>
2020-05-15 04:34:23 +0100

    Add 'categories' argument

--- R/bayes-factors.R
+++ R/bayes-factors.R
@@ -1,10 +1,11 @@
 
 # Multinomial - Dirichlet model: Bayes factor
 # ------------------------------------------
-bfactor.multinomial <- function(
+bfactor_multinomial <- function(
   x,
   null.par,
-  alpha = rep.int(1, length(null.par)),
+  categories = sort(unique(x)),
+  alpha = 1,
   transf = "level") {
 
   if(any(!is.atomic(x), !is.vector(x))){
@@ -17,21 +18,22 @@ bfactor.multinomial <- function(
     stop("Invalid argument: 'null.par' must be an atomic vector.")
   }
   if(any(!is.numeric(null.par), null.par >= 1, null.par <= 0, is.na(null.par), is.null(null.par))) {
-        stop("Invalid argument: all 'null.par' values must be between 0 and 1.")
+    stop("Invalid argument: all 'null.par' values must be between 0 and 1.")
   }
+  # if(sort(unique(x)) != sort(unique(categories))){
+  #   if(){}
+  #
+  # }
   if(any(!is.atomic(alpha), !is.vector(alpha))){
     stop("Invalid argument: 'alpha' must be an atomic vector.")
   }
-  if(any(!is.numeric(alpha), any(alpha < 0), any(is.na(alpha)), is.null(alpha))){
+  if(any(!is.numeric(alpha), alpha < 0, is.na(alpha), is.null(alpha))){
     stop("Invalid argument: 'alpha' must be non-negative.")
   }
   if(isFALSE(transf %in% c("level", "log", "log10"))){
     stop("Invalid argument: 'transf' must be either 'level', 'log' or 'log10'.")
   }
 
-  # if (length(null.par) == 1) {
-  #     stop("One parameter model. Use 'model = 'binomial'.")
-  #   } else {
   if (length(null.par) < length(alpha)) {
       stop("Invalid model specification: more hyper-parameters than null parameter values.")
     } else if (length(alpha) == 1){
@@ -42,9 +44,10 @@ bfactor.multinomial <- function(
         )
     }
 
-  counts <- table(x[!is.na(x)])
+  categories <- factor(categories, levels = categories , ordered = TRUE)
+  counts <- table(x[!is.na(x)])[levels(categories)]
 
-    bfactor <- function(counts, null.par, alpha) {
+  bfactor <- function(counts, null.par, alpha) {
 
       log.bfactor <- sum(counts * log(null.par)) + sum(lgamma(alpha)) + lgamma(sum(alpha + counts)) -
         lgamma(sum(alpha)) - sum(lgamma(alpha + counts))
@@ -63,7 +66,7 @@ bfactor.multinomial <- function(
 
 # Binomial - Beta model: Bayes factor
 # ------------------------------------------
-bfactor.binomial <- function(
+bfactor_binomial <- function(
   x,
   null.par,
   sucess,
@@ -103,8 +106,8 @@ bfactor.binomial <- function(
   }
 
   x <- x[!is.na(x)]
-  sucesses <- as.numeric(table(x == sucess)["TRUE"])
   n <- length(x)
+  sucesses <- as.numeric(table(x == sucess)["TRUE"])
 
     bfactor <- function(sucesses, null.par, a, b, n)  {
 
