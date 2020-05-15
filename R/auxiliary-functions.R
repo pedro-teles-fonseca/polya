d92edbd7d60f18c983268be90e7dbcf4aae5b3ba
Pedro Teles da Fonseca <pedro.teles.fonseca@outlook.com>
2020-05-15 20:03:16 +0100

    Move bfactor_to_prob to bfactor_.R

--- R/auxiliary-functions.R
+++ R/auxiliary-functions.R
@@ -1,11 +1,4 @@
 
-
-# Function to turn Bayes factors into posterior probabilities of the NULL
-
-bfactor.to.prob <- function(bf, pi_null = .5) {
-  ((1 + ((1 - pi_null) / pi_null) * (1 / bf))) ^ (-1)
-}
-
 # Function to compute the mode of a vector.
 
 getmode <- function(v) {
