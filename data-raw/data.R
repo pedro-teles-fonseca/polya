0844b3818ee0f3f55c678b12f3ed7a2aed96f954
Pedro Teles da Fonseca <pedro.teles.fonseca@outlook.com>
2020-05-15 20:04:56 +0100

    generate new data

---
+++ data-raw/data.R
@@ -0,0 +1,42 @@
+
+library(usethis)
+library(digit.analysis)
+
+data <- read.delim("data-raw/data.txt")
+
+for (col in seq_along(data)) {
+  assign(paste0(tolower(names(data)[col]), "_bl1"), msdigit(data[, col]))
+  assign(paste0(tolower(names(data)[col]), "_bl2"), smsdigit(data[, col]))
+}
+
+# ------------------------------------------------
+# Package data
+# ------------------------------------------------
+
+usethis::use_data(
+  austria_bl1,
+  belgium_bl1,
+  finland_bl1,
+  france_bl1,
+  germany_bl1,
+  greece_bl1,
+  ireland_bl1,
+  italy_bl1,
+  luxembourg_bl1,
+  netherlands_bl1,
+  portugal_bl1,
+  spain_bl1,
+  austria_bl2,
+  belgium_bl2,
+  finland_bl2,
+  france_bl2,
+  germany_bl2,
+  greece_bl2,
+  ireland_bl2,
+  italy_bl2,
+  luxembourg_bl2,
+  netherlands_bl2,
+  portugal_bl2,
+  spain_bl2,
+  overwrite = TRUE)
+
