3ae66f0b98f591a3f5dea1a3c568f71f7557cf5a
Pedro Teles da Fonseca <pedro.teles.fonseca@outlook.com>
2020-05-16 02:57:17 +0100

    chsq function for multinomial point nulls

--- R/bfactor_.R
+++ R/bfactor_.R
@@ -4,8 +4,8 @@
 
 bfactor_binomial <- function(
   x,
-  null.par,
   sucess,
+  null.par,
   a = 1,
   b = 1,
   transf = "level") {
@@ -13,8 +13,17 @@ bfactor_binomial <- function(
   if(any(!is.atomic(x), !is.vector(x), is.null(x))){
     stop("Invalid argument: 'x' must be an atomic vector.")
   }
-  if(isFALSE(typeof(x) %in% c("double", "integer", "character"))){
-    stop("Invalid argument: typeof(x) must be 'integer', 'double' or 'character'.")
+  if(isFALSE(typeof(x) %in% c("integer", "double", "character"))){
+    stop("Invalid argument: 'x' must be of type 'integer', 'double' or 'character'.")
+  }
+  if(any(is.na(sucess), is.null(sucess))){
+    stop("Invalid argument: 'sucess'  cant be missing nor null")
+  }
+  if(isFALSE(sucess %in% unique(x))){
+    stop("Invalid argument: 'sucess %in% unique(x)' must be TRUE.")
+  }
+  if(any(isFALSE(length(null.par) == 1), isFALSE(length(sucess) == 1))){
+    stop("Invalid argument: 'null.par' and 'sucess' must be of length 1")
   }
   if (any(!is.atomic(null.par), !is.vector(null.par))){
     stop("Invalid argument: 'null.par' must be an atomic vector.")
@@ -22,20 +31,14 @@ bfactor_binomial <- function(
   if(any(!is.numeric(null.par), null.par >= 1, null.par <= 0, is.na(null.par), is.null(null.par))) {
     stop("Invalid argument: all 'null.par' values must be between 0 and 1.")
   }
-  if(isFALSE(sucess %in% unique(x))){
-    stop("Invalid argument: 'sucess %in% unique(x)' must be TRUE.")
-  }
-  if(any(!is.atomic(a), !is.atomic(b), !is.vector(a), !is.vector(b))){
+  if(any(!is.atomic(a), !is.vector(a), !is.atomic(b), !is.vector(b))){
     stop("Invalid argument: 'a' and 'b' must be atomic vectors.")
   }
-  if(any(a <= 0, b <= 0, is.na(a), is.na(b), is.null(a), is.null(b))){
-    stop("Invalid argument: all values of 'a' and 'b' must be greather than 0.")
-  }
-  if(any(isFALSE(length(null.par) == 1), isFALSE(length(sucess) == 1))){
-    stop("Invalid argument: 'null.par' and 'sucess' must be of length 1")
-  }
   if(any(isFALSE(length(a) == 1), isFALSE(length(b) == 1))){
-    stop("Invalid argument: 'a' and 'b' must be of length 1")
+    stop("Invalid argument: 'a' and 'b' must be of length 1.")
+    }
+  if(any(a <= 0, is.na(a), is.null(a), b <= 0, is.na(b), is.null(b))){
+    stop("Invalid argument: 'a' and 'b' must be greather than 0.")
   }
   if(isFALSE(transf %in% c("level", "log", "log10"))){
     stop("Invalid argument: 'transf' must be either 'level', 'log' or 'log10'.")
@@ -45,22 +48,18 @@ bfactor_binomial <- function(
   n <- length(x)
   sucesses <- as.numeric(table(x == sucess)["TRUE"])
 
-  bfactor <- function(sucesses, null.par, a, b, n)  {
-
-    log.bfactor <- sucesses * log(null.par) + (n - sucesses) * log(1 - null.par) + lgamma(a) + lgamma(b) + lgamma(n + a + b) -
+  log.bfactor <- sucesses * log(null.par) + (n - sucesses) * log(1 - null.par) + lgamma(a) + lgamma(b) + lgamma(n + a + b) -
       (lgamma(a + b) + lgamma(n + a - sucesses) + lgamma(sucesses + a))
 
-    exp(log.bfactor)
-  }
+  bfactor <- exp(log.bfactor)
 
   if(transf == "log"){
-    log(bfactor(sucesses, null.par, a, b, n))
+    c("log(BF)" = log.bfactor)
   } else if(transf == "log10"){
-    log10(bfactor(sucesses, null.par, a, b, n))
+    c("log10(BF)" = log10(bfactor))
   } else {
-    bfactor(sucesses, null.par, a, b, n)
+    c("BF" = bfactor)
   }
-
 }
 
 # ----------------------------------------------------------------------------------------
@@ -69,8 +68,8 @@ bfactor_binomial <- function(
 
 bfactor_multinomial <- function(
   x,
+  categories,
   null.par,
-  categories = sort(unique(x)),
   alpha = 1,
   transf = "level") {
 
@@ -89,8 +88,8 @@ bfactor_multinomial <- function(
   if(any(is.na(categories), is.null(categories))){
     stop("Invalid argument: 'categories' must be a vector of the same lenght as 'x'")
   }
-  if(typeof(categories) != typeof(x)){
-    stop("Invalid argument: 'categories' must be a vector of the same type as 'x'")
+  if(isFALSE(typeof(categories) %in% c("double", "integer", "character"))){
+    stop("Invalid argument: typeof(categories) must be 'integer', 'double' or 'character'.")
   }
   if(any(sort(unique(x)) != sort(unique(categories)))){
       stop("Invalid argument: the unique values of 'x' and 'categories' must be the same.")
@@ -114,7 +113,7 @@ bfactor_multinomial <- function(
         )
     }
 
-  categories <- factor(categories, levels = categories , ordered = TRUE)
+  categories <- factor(categories, levels = categories)
   counts <- table(x[!is.na(x)])[levels(categories)]
 
   bfactor <- function(counts, null.par, alpha) {
@@ -126,11 +125,11 @@ bfactor_multinomial <- function(
     }
 
     if(transf == "log"){
-      log(bfactor(counts, null.par, alpha))
+      c("log(BF)" =  log(bfactor(counts, null.par, alpha)))
     } else if(transf == "log10"){
-      log10(bfactor(counts, null.par, alpha))
+      c("log10(BF)" =  log10(bfactor(counts, null.par, alpha)))
     } else {
-      bfactor(counts, null.par, alpha)
+      c("BF" =  bfactor(counts, null.par, alpha))
     }
 }
 
@@ -140,10 +139,32 @@ bfactor_multinomial <- function(
 
 bfactor_interpret <- function(bf) {
 
+  bf <- unname(bf)
+
   evidence <- ifelse(bf < 1, "Negative",
-    ifelse(bf < 3.2, "Weak",
-      ifelse(bf < 10, "Substantial",
-        ifelse(bf < 100, "Strong", "Decisive")
+                ifelse(bf < 3.2, "Weak",
+                  ifelse(bf < 10, "Substantial",
+                    ifelse(bf < 100, "Strong",
+                      "Decisive")
+      )
+    )
+  )
+  c("Evidence" = evidence)
+}
+
+# ----------------------------------------------------------------------------------------
+# Function to interpret the evidence provided by the data in favour of the null hypothesis
+# ----------------------------------------------------------------------------------------
+
+bfactor_log_interpret <- function(l_bf, base = exp(1)) {
+
+  l_bf <- unname(l_bf)
+
+  evidence <- ifelse(l_bf < log(1, base = base), "Negative",
+    ifelse(l_bf < log(3.2, base = base), "Weak",
+      ifelse(l_bf < log(10, base = base), "Substantial",
+        ifelse(l_bf < log(100, base = base), "Strong",
+          "Decisive")
       )
     )
   )
@@ -169,14 +190,12 @@ bfactor_to_prob <- function(bf, pi_null = .5) {
     stop("Error: 'pi_null' must be between zero and one.")
   }
 
-c("probability",  ((1 + ((1 - pi_null) / pi_null) * (1 / bf))) ^ (-1))
+  c("P(H0|X)" =  ((1 + ((1 - pi_null) / pi_null) * (1 / unname(bf)))) ^ (-1))
 
 }
 
-# bfactor_multinomial(x = austria_bl1, null.par = theta_benford(1), transf = "log10")
-# bfactor_to_prob(bfactor_multinomial(x = austria_bl1, null.par = theta_benford(1), transf = "log10"))
-# bfactor_interpret(bfactor_multinomial(x = austria_bl1, null.par = theta_benford(1), transf = "log10"))
-# #
-#
+
+
+
 
 
