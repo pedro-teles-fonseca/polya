85e32a1cba56ea46414d5e773817e45336a1558f
Pedro Teles da Fonseca <pedro.teles.fonseca@outlook.com>
2020-08-11 22:22:38 +0100

    update tests

---
+++ tests/testthat/helper-aibf.R
@@ -0,0 +1,28 @@
+
+aibf_multinomial_mts <- function(x, categories, null_par){
+
+  x <- x[!is.na(x)]
+  n <- length(x)
+  k <- length(categories)
+  counts <- table(x)
+
+  exp(lgamma(k) - lgamma(n) + sum(lgamma(counts)) + sum((1-counts) * log(null_par)))
+
+}
+
+aibf_multinomial_mts_2 <- function(x, categories, null_par){
+
+  x <- x[!is.na(x)]
+  n <- length(x)
+  k <- length(categories)
+  counts <- table(x)
+
+  exp(sum((1-counts) * log(null_par)) + lmbeta(counts) - lmbeta(rep(1, k)))
+
+}
+
+
+
+
+
+
---
+++ tests/testthat/test-aibf.R
@@ -0,0 +1,36 @@
+
+context("aibf")
+
+test_that("aibf helpers", {
+  expect_equal(
+    aibf_multinomial_mts_2(x = austria_bl1, categories = 1:9, null_par = theta_benford(1)),
+    aibf_multinomial_mts(x = austria_bl1, categories = 1:9, null_par = theta_benford(1))
+  )
+})
+
+test_that("aibf multinomial haldane w/ mts", {
+  expect_equal(
+    ibf(x = austria_bl1, categories = 1:9, null_par = theta_benford(1), prior = "haldane", in_favour = "h1", method = "mts"),
+    aibf_multinomial_mts(x = austria_bl1, categories = 1:9, null_par = theta_benford(1))
+    )
+})
+
+test_that("aibf and gibf with mts", {
+  expect_equal(
+    ibf(x = austria_bl1, categories = 1:9, null_par = theta_benford(1), prior = "haldane", type = "geometric", in_favour = "h1", method = "mts"),
+    ibf(x = austria_bl1, categories = 1:9, null_par = theta_benford(1), prior = "haldane", type = "arithmetic", in_favour = "h1", method = "mts")
+  )
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
