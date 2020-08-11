9b32638dde1b6a1e8f3746f6ec6605cc0b1efa83
Pedro Teles da Fonseca <pedro.teles.fonseca@outlook.com>
2020-08-11 22:22:45 +0100

    aibf

---
+++ R/aibf.R
@@ -0,0 +1,59 @@
+
+ibf <- function(
+  x,
+  categories,
+  null_par,
+  prior = "haldane",
+  hyper_par = 1,
+  type = "arithmetic",
+  method = "smts",
+  k = 2,
+  in_favour = "H1") {
+
+  data <- x[!is.na(x)]
+  n <- length(data)
+  l <- k * n
+
+  b10 <- bfactor_multinomial(x = data, categories = categories, null_par = null_par, prior = prior, in_favour = "H1")
+
+  if(method == "smts"){
+
+    x_l <- list()
+    l_len <- vector(mode = "numeric")
+
+    for (i in seq_len(l)){
+
+      x_l[[i]] <- vector(mode = "numeric")
+
+      while (length(table(x_l[[i]])) < length(categories)) {
+        x_l[[i]] <- c(x_l[[i]], sample(data, size = 1, replace = TRUE))
+      }
+    }
+
+    b01 <- sapply(X = x_l, FUN = bfactor_multinomial, categories = categories, prior = prior, in_favour = "H0")
+  }
+
+  if(method == "mts"){
+
+    b01 <- bfactor_multinomial(x = categories, categories = categories, null_par = null_par, prior = prior , in_favour = "H0")
+
+  }
+
+  ibf <- switch (type,
+    "arithmetic" = , "a" = b10 * mean(b01),
+    "geometric" = , "g" = b10 * exp(mean(log(b01)))
+  )
+
+  switch(
+    tolower(in_favour),
+    "h0" = , "null" = 1/ibf,
+    "h1" = , "alternative" = ibf)
+}
+
+
+
+
+
+
+
+
