72ac17fe26cf62439ee4f67c2c03d5e2760a6e01
Pedro Teles da Fonseca <pedro.teles.fonseca@outlook.com>
2020-09-05 00:16:45 +0100

    update helper

--- tests/testthat/helper-bl.R
+++ tests/testthat/helper-bl.R
@@ -1,4 +1,14 @@
 
+theta_benford <- function(d) {
 
-theta_bl1 <- sapply(1:9, function(x){log10(1+1/(x))})
-theta_bl2 <- sapply(0:9, function(x){sum(log10(1+1/(10*(1:9)+x)))})
+  if (d == 1){
+    log10(1 + 1/1:9)
+  } else if (d == 2){
+    sapply(0:9, function(x){sum(log10(1+1/(10*(1:9)+x)))})
+  } else {
+    stop("Invalid argument: 'd' must be either 1 or 2.")
+  }
+}
+
+theta_bl1 <- theta_benford(1)
+theta_bl2 <- theta_benford(2)
