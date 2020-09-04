7155e6bcbf610fb9264c6f8296dceebfbd4e470e
Pedro Teles da Fonseca <pedro.teles.fonseca@outlook.com>
2020-09-05 00:05:25 +0100

    update tests

--- tests/testthat/test-aibf.R
+++ tests/testthat/test-aibf.R
@@ -12,7 +12,7 @@ test_that("aibf multinomial haldane w/ mts", {
   expect_equal(
     ibf(x = austria_bl1, categories = 1:9, null_par = theta_benford(1), prior = "haldane", in_favour = "h1", method = "mts"),
     aibf_multinomial_mts(x = austria_bl1, categories = 1:9, null_par = theta_benford(1))
-    )
+  )
 })
 
 test_that("aibf and gibf with mts", {
--- tests/testthat/test-cumulative_.R
+++ tests/testthat/test-cumulative_.R
@@ -3,28 +3,28 @@ context("Cumulative BF - multinomial")
 
 test_that("1 sample test 1", {
   expect_equal(
-    cumulative_bf_multinomial(samples = list(austria_bl1), categories = 1:9, null_par = theta_benford(1), hyper_par = rep(1, 9)),
+    sapply_bf_multinomial(samples = list(austria_bl1), categories = 1:9, null_par = theta_benford(1), hyper_par = rep(1, 9)),
     bf_multinomial(x = austria_bl1, categories = 1:9, null_par = theta_benford(1), hyper_par = rep(1, 9)),
   )
 })
 
 test_that("1 sample test 1", {
   expect_equal(
-    cumulative_bf_multinomial(samples = list(austria_bl1), categories = 1:9, null_par = theta_benford(1), hyper_par = rep(1, 9)),
+    sapply_bf_multinomial(samples = list(austria_bl1), categories = 1:9, null_par = theta_benford(1), hyper_par = rep(1, 9)),
     fbf_multinomial(x = austria_bl1, categories = 1:9, null_par = theta_benford(1), hyper_par = rep(1, 9), frac = 0)
   )
 })
 
 test_that("1 sample test 1", {
   expect_equal(
-    cumulative_bf_multinomial(samples = list(austria_bl1), categories = 1:9, null_par = theta_benford(1), hyper_par = rep(1, 9)),
+    sapply_bf_multinomial(samples = list(austria_bl1), categories = 1:9, null_par = theta_benford(1), hyper_par = rep(1, 9)),
     fbf_multinomial(x = austria_bl1, categories = 1:9, null_par = theta_benford(1), hyper_par = rep(1, 9), m = 0, robust = "yes")
   )
 })
 
 test_that("2 samples", {
   expect_equal(
-    cumulative_bf_multinomial(
+    sapply_bf_multinomial(
       samples = list(austria_bl1, finland_bl1),
       categories = 1:9,
       null_par = theta_benford(1),
@@ -41,7 +41,7 @@ context("Cumulative FBF - multinomial")
 
 test_that("1 samples", {
   expect_equal(
-    cumulative_fbf_multinomial(
+    sapply_fbf_multinomial(
       samples = list(austria_bl1), categories = 1:9, null_par = theta_benford(1), hyper_par = rep(1, 9)),
     fbf_multinomial(x = austria_bl1, categories = 1:9, null_par = theta_benford(1), hyper_par = rep(1, 9))
   )
@@ -49,7 +49,7 @@ test_that("1 samples", {
 
 test_that("2 samples", {
   expect_equal(
-    cumulative_fbf_multinomial(samples = list(austria_bl1, finland_bl1), 1:9, theta_benford(1), rep(1, 9)),
+    sapply_fbf_multinomial(samples = list(austria_bl1, finland_bl1), 1:9, theta_benford(1), rep(1, 9)),
     c(fbf_multinomial(austria_bl1, 1:9, theta_benford(1), 1), fbf_multinomial(finland_bl1, 1:9, theta_benford(1), rep(1, 9)))
   )
 })
